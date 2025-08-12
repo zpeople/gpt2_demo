5f9a30be3da8e3b95e88758fd81a9efd5498a68c
zpeople <6829549@qq.com>
2025-08-12 13:59:51 +0800

    add tranin process

---
+++ src/tool.py
@@ -0,0 +1,10 @@
+def skip_execution(skip=True):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if not skip:
+                return func(*args, **kwargs)
+            # 如果skip为True，则不执行函数，返回None或提示
+            print(f"函数 {func.__name__} 已跳过执行")
+            return None
+        return wrapper
+    return decorator
