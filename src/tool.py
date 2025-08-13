1e1240f5c81d9f68990db0aa63416f73f8000143
zpeople <6829549@qq.com>
2025-08-13 11:57:51 +0800

    add tensorboard

--- src/tool.py
+++ src/tool.py
@@ -8,3 +8,15 @@ def skip_execution(skip=True):
             return None
         return wrapper
     return decorator
+
+
+def train_execution(train=True):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if train:
+                return func(*args, **kwargs)
+            # 如果skip为True，则不执行函数，返回None或提示
+            print(f"跳过训练 {func.__name__} 已跳过执行")
+            return None
+        return wrapper
+    return decorator
